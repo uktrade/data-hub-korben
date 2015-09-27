8aca26cce0fe85f54e8dff9ab678d6d56dd0dd26
bmcorser <bmcorser@gmail.com>
2015-09-27 11:03:53 +0100

    Slightly less dodgy config module

--- bmu/config.py
+++ bmu/config.py
@@ -1,5 +1,52 @@
-import yaml
+import os as __os
+import yaml as __yaml
 
-with open('config.yaml', 'r') as config_fh:
-    for name, value in yaml.load(config_fh.read()).items():
+__config_spec = {
+    #                                       default
+    #                             read from  value
+    #   name            required     env     /
+    #    |                     \      |     /
+    'github_user':           (True, True, None),
+    'github_token':          (True, True, None),
+    'github_webhook_secret': (True, True, None),
+    'repos':                 (True, False, None),
+    'namespace':             (True, False, 'bmu'),
+}
+
+
+with open('config.yaml', 'r') as __config_yaml_fh:
+    __config_yaml = __yaml.load(__config_yaml_fh.read())
+
+
+def __set_config(name, value=None):
+    '''
+    Get a config value from a file in the repo root, or from an evironment
+    variable with a matching name
+    '''
+    try:
+        required, read_env, default = __config_spec['name']
+    except KeyError:
+        raise NameError(
+            "`{0}` is not a recongised configuration key".format(name)
+        )
+    value = __config_yaml.get(name)
+    if read_env:
+        here = __os.path.dirname(__file__)
+        try:
+            with open(__os.path.join(here, '..', name.upper()), 'r') \
+            as __constant_fh:
+                value = __constant_fh.read().rstrip('\n')
+        except IOError as exc:
+            if exc.errno == 2:
+                value = __os.environ[name.upper()]
+    if not value and required and default:
+        value = default
+    if value:
         locals()[name] = value
+    elif required:
+        raise NameError(
+            "Configuration key `{0}` is required".format(name)
+        )
+
+for name in __config_spec.keys():
+    __set_config(name)
