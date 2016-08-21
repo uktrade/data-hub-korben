7d63230b71c2a1c90f86766e81eab712ca6f0d51
Zac Tolley <zac@thetolleys.com>
2016-08-21 15:52:58 +0100

    continue to try fix static files

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -xe
+mkdir /usr/src/app/datahubapi/static
 mkdir /usr/src/app/datahubapi/staticfiles
 python /usr/src/app/manage.py migrate                  # Apply database migrations
 python /usr/src/app/manage.py collectstatic --noinput  # Collect static files
