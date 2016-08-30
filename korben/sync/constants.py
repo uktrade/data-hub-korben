324cbe07fda3239152f430ded77e8c823ec017a7
bmcorser <bmcorser@gmail.com>
2016-08-30 17:03:47 +0000

    Start hacking on polling code

---
+++ korben/sync/constants.py
@@ -0,0 +1,4 @@
+TYPE_KEY = '{http://schemas.microsoft.com/ado/2007/08/dataservices/metadata}type'
+CONTENT_TAG = '{http://www.w3.org/2005/Atom}content'
+LINK_TAG = '{http://www.w3.org/2005/Atom}link'
+ENTRY_TAG = '{http://www.w3.org/2005/Atom}entry'
--- korben/sync/poll.py
+++ korben/sync/poll.py
@@ -1,12 +1,32 @@
-from ..cdms_api.rest.api import CDMSRestApi
+from lxml import etree
 import sqlalchemy as sqla
+from ..cdms_api.rest.api import CDMSRestApi
+from . import resp_csv
+from . import constants
+from . import utils
 
-def entity():
-    cdms_client = CDMSRestApi()
-
-def main(resp_dir, entity_name):
+def main(entity_name):
+    cdms_api = CDMSRestApi()
     engine = sqla.create_engine('postgresql://localhost/cdms_psql')
     metadata = sqla.MetaData(bind=engine)
     metadata.reflect()
-    tempfiles, proc = main(metadata, resp_dir, entity_name)
-    proc.wait()
+    table = metadata.tables[entity_name + 'Set']
+    col_names = [x.name for x in table.columns]
+    # assume a single column
+    primary_key = next(col.name for col in table.primary_key.columns.values())
+    resp = cdms_api.list(entity_name, order_by='ModifiedOn')
+    rows = []
+    for entry in etree.fromstring(resp.content).iter(constants.ENTRY_TAG):
+        rows.append(utils.entry_row(col_names, None, entry))
+    for row in rows:
+        in_db = (
+            table.select()
+                 .where(table.columns[primary_key] == row[primary_key])
+                 .execute()
+                 .fetchone()
+        )
+        if in_db:
+            import ipdb;ipdb.set_trace()
+            pass
+
+        pass
