8ae9eb6ad33562fd49b3ae6ac16f02d3fdbf4d2d
bmcorser <bmcorser@gmail.com>
2016-08-30 23:26:29 +0100

    Chuck inspection command in

--- sync/__init__.py
+++ sync/__init__.py
@@ -1,10 +1,11 @@
 'Namespace for sync module'
-from . import poll, traverse, scrape, clean_cache  # NOQA
+from . import poll, traverse, scrape, clean_cache, inspect_xml  # NOQA
 
 COMMANDS = {
     'scrape': scrape.main,
     'poll': poll.main,
     'clean': clean_cache.main,
+    'inspect': inspect_xml.main,
 }
 
 __all__ = ('COMMANDS', 'poll', 'traverse', 'scrape')
