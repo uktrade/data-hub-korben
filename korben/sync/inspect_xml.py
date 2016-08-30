090879eb2a84d4df2a110945a1c1cb3fa9a5dd1b
bmcorser <bmcorser@gmail.com>
2016-08-30 23:26:29 +0100

    Chuck inspection command in

--- korben/sync/inspect_xml.py
+++ korben/sync/inspect_xml.py
@@ -6,21 +6,12 @@ from . import constants
 
 
 
-def main(entity_name, start, number=10):
+def main(cache_dir, entity_name, start=0, number=10):
     for page in map(str, range(start, start + (number * 50), 50)):
-        fh = open(os.path.join('../cache/list', entity_name, page), 'rb')
+        fh = open(os.path.join(cache_dir, entity_name, page), 'rb')
         resp = pickle.load(fh)
         fh.close()
         print('===========================')
         print("{0} ({1})".format(entity_name, page))
         print('===========================')
         print(resp.content.decode(resp.encoding))
-
-if __name__ == '__main__':
-    entity_name = sys.argv[1]
-    start = int(sys.argv[2])
-    try:
-        number = int(sys.argv[3])
-        main(entity_name, start, number)
-    except IndexError:
-        main(entity_name, start)
