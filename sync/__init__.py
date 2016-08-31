9c1b63036a43879e928a16522494e5f774ca6615
bmcorser <bmcorser@gmail.com>
2016-08-31 01:15:49 +0100

    Add populate command

--- sync/__init__.py
+++ sync/__init__.py
@@ -1,11 +1,13 @@
 'Namespace for sync module'
-from . import poll, traverse, scrape, clean_cache, inspect_xml  # NOQA
+from . import poll, traverse, scrape  # NOQA
+from . import clean_cache, inspect_xml, populate
 
 COMMANDS = {
     'scrape': scrape.main,
     'poll': poll.main,
     'clean': clean_cache.main,
     'inspect': inspect_xml.main,
+    'populate': populate.main,
 }
 
 __all__ = ('COMMANDS', 'poll', 'traverse', 'scrape')
