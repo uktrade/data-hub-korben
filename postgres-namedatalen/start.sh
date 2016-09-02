a516da0247e76a4fdbb16758ca5f0adee8bb27b1
Mateusz Lapsa-Malawski <m@cr3.io>
2016-09-02 16:54:03 +0100

    1st iteration of postgres-namedatalen

--- postgres-namedatalen/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ -n /var/lib/postgresql/data/sss ];
-    /usr/local/pgsql/bin/initdb /usr/local/var/postgres
-done
-
-/usr/local/pgsql/bin/pg_ctl -D /usr/local/var/postgres -l logfile start
