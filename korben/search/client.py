55a0551260074306a59cbdb11c0c9be720ac53bb
bmcorser <bmcorser@gmail.com>
2016-09-12 17:18:49 +0100

    Axe AWS ES client

--- korben/search/client.py
+++ korben/search/client.py
@@ -1,24 +1,6 @@
-from elasticsearch import Elasticsearch, RequestsHttpConnection
-from requests_aws4auth import AWS4Auth
+from elasticsearch import Elasticsearch
 from korben import config
 
-try:
-    awsauth = AWS4Auth(
-        config.es_access,
-        config.es_secret,
-        config.es_region,
-        'es',
-    )
-
-    client = Elasticsearch(
-        hosts=[{'host': config.es_host, 'port': config.es_port}],
-        http_auth=awsauth,
-        use_ssl=True,
-        verify_certs=True,
-        connection_class=RequestsHttpConnection
-    )
-except AttributeError:
-    client = Elasticsearch(
-        hosts=[{'host': config.es_host, 'port': config.es_port}],
-        connection_class=RequestsHttpConnection
-    )
+client = Elasticsearch(
+    hosts=[{'host': config.es_host, 'port': config.es_port}]
+)
