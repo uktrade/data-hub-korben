2c321005385354c0546d6820503b4c00ee30c119
bmcorser <bmcorser@gmail.com>
2016-09-20 08:19:56 +0100

    More informative logging

--- korben/services/db.py
+++ korben/services/db.py
@@ -22,6 +22,9 @@ def poll_for_metadata(url):
             if len(metadata.tables) == 0:
                 # initial set up case, before database have tables created.
                 # just keep polling
+                LOGGER.info(
+                    "Waiting {0}s for tables in db {1}".format(interval, url)
+                )
                 time.sleep(interval)
                 interval += 2
             else:
@@ -48,7 +51,7 @@ def poll_for_connection(url):
             break
         except Exception as exc:
             LOGGER.error(exc)
-            LOGGER.info("Waiting {0}s for database".format(interval))
+            LOGGER.info("Waiting {0}s for database {1}".format(interval, url))
             time.sleep(interval)
             interval += 2
     return connection
