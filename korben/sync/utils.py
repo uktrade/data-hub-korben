903e3f4876bd8ba3b88ce88cfd08395425f3ffc6
bmcorser <bmcorser@gmail.com>
2016-09-27 12:23:12 +0100

    Remove unused logging class

--- korben/sync/utils.py
+++ korben/sync/utils.py
@@ -102,64 +102,6 @@ def link_fkey_map(table, entry):
     return link_map
 
 
-class MultiProcessingLog(logging.Handler):
-    def __init__(self, name, mode, maxsize, rotate):
-        logging.Handler.__init__(self)
-
-        self._handler = RotatingFileHandler(name, mode, maxsize, rotate)
-        self.queue = multiprocessing.Queue(-1)
-
-        t = threading.Thread(target=self.receive)
-        t.daemon = True
-        t.start()
-
-    def setFormatter(self, fmt):
-        logging.Handler.setFormatter(self, fmt)
-        self._handler.setFormatter(fmt)
-
-    def receive(self):
-        while True:
-            try:
-                record = self.queue.get()
-                self._handler.emit(record)
-            except (KeyboardInterrupt, SystemExit):
-                raise
-            except EOFError:
-                break
-            except:
-                traceback.print_exc(file=sys.stderr)
-
-    def send(self, s):
-        self.queue.put_nowait(s)
-
-    def _format_record(self, record):
-        # ensure that exc_info and args
-        # have been stringified.  Removes any chance of
-        # unpickleable things inside and possibly reduces
-        # message size sent over the pipe
-        if record.args:
-            record.msg = record.msg % record.args
-            record.args = None
-        if record.exc_info:
-            dummy = self.format(record)
-            record.exc_info = None
-
-        return record
-
-    def emit(self, record):
-        try:
-            s = self._format_record(record)
-            self.send(s)
-        except (KeyboardInterrupt, SystemExit):
-            raise
-        except:
-            self.handleError(record)
-
-    def close(self):
-        self._handler.close()
-        logging.Handler.close(self)
-
-
 def parse_atom_entries(cache_dir, entity_name, name, path=None):
     'Parse <entry> elements from an XML document in Atom format'
     if path is None:
