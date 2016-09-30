5b86300140075ca613fd4a2e22c7b81522b37a50
bmcorser <bmcorser@gmail.com>
2016-09-30 15:46:07 +0100

    Add docstring to fixture

--- test/conftest.py
+++ test/conftest.py
@@ -11,6 +11,7 @@ TEST_PROP_KV_MAP = {
 
 @pytest.yield_fixture
 def odata_sync_utils():
+    'Patch sync.utils for tests running against plain OData service'
     original_prop_kv_map = copy.deepcopy(sync_utils.PROP_KV_MAP)
     sync_utils.PROP_KV_MAP = TEST_PROP_KV_MAP
     yield sync_utils
