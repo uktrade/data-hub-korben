053767bf40027489533316fa6c32e0389d6243d9
bmcorser <bmcorser@gmail.com>
2016-10-04 17:33:34 +0000

    Assert 404s are in JSON

--- test/unit/bau/test_exceptions.py
+++ test/unit/bau/test_exceptions.py
@@ -1,3 +1,19 @@
-def test_exceptions_json(test_app):
-    import ipdb;ipdb.set_trace()
-    pass
+import pytest
+
+
+def test_404_json(test_app):
+    '404 should be JSON'
+    resp = test_app.get('/', status=404)
+    assert resp.content_type == 'application/json'
+    assert resp.json_body == {'message': '/'}
+
+
+@pytest.mark.parametrize('route_name', ('create', 'update'))
+def test_bad_table_404_json(test_app, route_name):
+    'Unsupported table name should 404 with JSON'
+    not_a_table = 'cake'
+    resp = test_app.post_json(
+        '/{0}/{1}'.format(route_name, not_a_table), {}, status=404
+    )
+    assert resp.content_type == 'application/json'
+    assert not_a_table in resp.json_body['message']
