be1fe1086b849a63db1982f93b2e43d511f76139
bmcorser <bmcorser@gmail.com>
2016-10-18 16:03:04 +0100

    Some work on mapping

--- korben/sync/django_initial.py
+++ korben/sync/django_initial.py
@@ -30,7 +30,7 @@ def fetch_missing(metadata, missing):
         guids = missing[django_name]
         if not guids:
             continue
-        import ipdb;ipdb.set_trace()
+        LOGGER.info('Downloading %s entries for %s', len(guids), django_name)
         table = metadata.tables[django_name]
         get_fn = functools.partial(utils.get_django, client, table.name)
         results, missing = etl.load.to_sqla_table_idempotent(
@@ -55,4 +55,5 @@ def main(client=None):
             guids = map(operator.itemgetter(primary_key), rows)
             results, missing =\
                 etl.main.from_odata(odata_table, tuple(guids), idempotent=True)
-            fetch_missing(django_metadata, missing)
+            if missing:
+                fetch_missing(django_metadata, missing)
