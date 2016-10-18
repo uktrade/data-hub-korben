331050d9d24f1fd224d2ffecdcd364040cd31459
bmcorser <bmcorser@gmail.com>
2016-10-18 13:35:13 +0100

    `korben sync django` fills integrity holes
     - Add "Undefined" constant to Django fixtures, Korben
     - Add to specs to use undefined const

--- korben/sync/django_initial.py
+++ korben/sync/django_initial.py
@@ -1,3 +1,4 @@
+import functools
 import logging
 import operator
 
@@ -5,15 +6,13 @@ import sqlalchemy as sqla
 
 from korben import services
 from korben import etl
+from korben.cdms_api.rest.api import CDMSRestApi
 from . import utils
 
-# temporary crap
 LOGGER = logging.getLogger('korben.sync.django_initial')
 
 
-def main(client=None):
-    odata_metadata = services.db.get_odata_metadata()
-    django_metadata = services.db.get_django_metadata()
+def django_tables_dep_order(django_metadata):
     odata_django = []
     django_fkey_deps = etl.utils.fkey_deps(django_metadata)
     for depth in sorted(django_fkey_deps.keys()):
@@ -22,7 +21,28 @@ def main(client=None):
                 odata_django.append(
                     (etl.spec.DJANGO_LOOKUP[table_name], table_name)
                 )
-    for odata_name, django_name in odata_django:
+    return odata_django
+
+
+def fetch_missing(metadata, missing):
+    client = CDMSRestApi()
+    for _, django_name in django_tables_dep_order(metadata):
+        guids = missing[django_name]
+        if not guids:
+            continue
+        import ipdb;ipdb.set_trace()
+        table = metadata.tables[django_name]
+        get_fn = functools.partial(utils.get_django, client, table.name)
+        results, missing = etl.load.to_sqla_table_idempotent(
+            table, map(get_fn, guids)
+        )
+        pass
+
+
+def main(client=None):
+    odata_metadata = services.db.get_odata_metadata()
+    django_metadata = services.db.get_django_metadata()
+    for odata_name, django_name in django_tables_dep_order(django_metadata):
         LOGGER.info('Dumping %s -> %s', odata_name, django_name)
         odata_table = odata_metadata.tables[odata_name]
         primary_key = etl.utils.primary_key(odata_table)
@@ -33,4 +53,6 @@ def main(client=None):
         )
         for rows in chunks:
             guids = map(operator.itemgetter(primary_key), rows)
-            etl.main.from_odata(odata_table, tuple(guids), idempotent=True)
+            results, missing =\
+                etl.main.from_odata(odata_table, tuple(guids), idempotent=True)
+            fetch_missing(django_metadata, missing)
--- korben/sync/utils.py
+++ korben/sync/utils.py
@@ -4,6 +4,8 @@ import os
 import sqlalchemy as sqla
 from sqlalchemy.sql import functions as sqla_func
 
+from korben.etl import transform, spec
+
 LOGGER = logging.getLogger('korben.sync.utils')
 CHUNKSIZE = 1000
 
@@ -19,10 +21,18 @@ def file_leaf(*args):
     return path
 
 
-def select_chunks(execute, basetable, select):
+def select_chunks(execute, basetable, select, chunksize=CHUNKSIZE):
     'Generator yielding chunks of a select'
     count_q = sqla.select([sqla.func.count()]).select_from(basetable)
     count = execute(count_q).scalar()
-    for offset in range(0, count, CHUNKSIZE):
+    for offset in range(0, count, chunksize):
         LOGGER.info('Evaluating chunk %s', offset)
-        yield execute(select.offset(offset).limit(CHUNKSIZE)).fetchall()
+        yield execute(select.offset(offset).limit(chunksize)).fetchall()
+
+
+def get_django(client, django_tablename, guid):
+    'Get a single object from Django table name and GUID'
+    odata_tablename = spec.DJANGO_LOOKUP[django_tablename]
+    resp = client.get(odata_tablename, "guid'{0}'".format(guid))
+    odata_dict = resp.json()['d']
+    return transform.odata_to_django(odata_tablename, odata_dict)
