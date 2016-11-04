0f8409cebe99e364b0e8557c186db75f0b2bd21e
bmcorser <bmcorser@gmail.com>
2016-11-04 11:07:16 +0000

    Just return if parsing date fails
    Probably not the best policy

--- korben/cdms_api/rest/utils.py
+++ korben/cdms_api/rest/utils.py
@@ -19,6 +19,8 @@ def cdms_datetime_to_datetime(value):
         parsed_val = int(match.group(1))
         parsed_val = datetime.datetime.utcfromtimestamp(parsed_val / 1000)
         return parsed_val.replace(tzinfo=datetime.timezone.utc)
+    else:
+        return value
 
 
 def datetime_to_cdms_datetime(value):
