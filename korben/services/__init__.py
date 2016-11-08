eed95a74b3f732070d02129c74bfe3cc8795c169
bmcorser <bmcorser@gmail.com>
2016-11-08 14:28:49 +0000

    Typo

--- korben/services/__init__.py
+++ korben/services/__init__.py
@@ -7,7 +7,7 @@ from . import db_manager
 
 redis_url = urllib.parse.urlparse(config.redis_url)
 redis = Redis(
-    url=redis_url.hostname, port=redis_url.port, decode_responses=True
+    host=redis_url.hostname, port=redis_url.port, decode_responses=True
 )
 db = db_manager.DatabaseManager()
 es = Elasticsearch(hosts=[{'host': config.es_host, 'port': config.es_port}])
