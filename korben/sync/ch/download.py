1106367a8455b035a113986a9209e869a9d79307
bmcorser <bmcorser@gmail.com>
2017-01-10 11:41:07 +0000

    Revert manually

--- korben/sync/ch/download.py
+++ korben/sync/ch/download.py
@@ -27,16 +27,17 @@ def zips(names):
     base_url = urllib.parse.urlparse(constants.DOWNLOAD_URL)
     retval = []
     for name in names:
-        zip_path = sync_utils.file_leaf(
+        cache_path = sync_utils.file_leaf(
             constants.CACHE_PATH, 'ch', 'zip', name
         )
-        if redis_bytes.get(zip_path):
-            retval.append(zip_path)
+        if zipfile.is_zipfile(cache_path):
+            retval.append(cache_path)
             continue
         zip_url = "{0.scheme}://{0.hostname}/{1}".format(base_url, name)
         zip_resp = requests.get(zip_url)
-        redis_bytes.set(zip_path, zip_resp.content)
-        retval.append(zip_path)
+        with open(cache_path, 'wb') as zip_fh:
+            zip_fh.write(zip_resp.content)
+        retval.append(cache_path)
     return retval
 
 
@@ -44,17 +45,15 @@ def extract(zip_paths):
     'Extract first file from each of the passed paths (to zip files)'
     csv_paths = []
     for zip_path in zip_paths:
-        downloaded_file = io.BytesIO(redis_bytes.get(zip_path))
-        with zipfile.ZipFile(downloaded_file) as zf_cache_check:
+        with zipfile.ZipFile(zip_path) as zf_cache_check:
             name = zf_cache_check.filelist[0].filename
-        csv_path = sync_utils.file_leaf(
+        cache_path = sync_utils.file_leaf(
             constants.CACHE_PATH, 'ch', 'csv', name
         )
-        if redis_bytes.get(csv_path):  # assume that if it exists all is well
-            csv_paths.append(csv_path)
+        if os.path.isfile(cache_path):  # assume that if it exists all is well
+            csv_paths.append(cache_path)
             continue
-        with zipfile.ZipFile(downloaded_file) as zf_extract:
-            file_bytes = zf_extract.read(name)
-            redis_bytes.set(csv_path, file_bytes)
-            csv_paths.append(csv_path)
+        with zipfile.ZipFile(zip_path) as zf_extract:
+            zf_extract.extract(name, os.path.dirname(cache_path))
+            csv_paths.append(cache_path)
     return csv_paths
