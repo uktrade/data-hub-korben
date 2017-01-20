e46fdb4d656b39c929f469696bf35c1a7cb68119
bmcorser <bmcorser@gmail.com>
2017-01-20 11:43:19 +0000

    Add script for counting databases on Flynn

---
+++ scripts/db-count.py
@@ -0,0 +1,19 @@
+import pprint
+from korben import config
+from korben.services import db
+
+odata_connection = db.poll_for_connection(config.odata_database_url)
+django_connection = db.poll_for_connection(config.django_database_url)
+
+
+COUNT_ALL_SQL = '''
+ANALYZE;
+SELECT relname, n_live_tup
+        FROM pg_stat_user_tables
+        WHERE n_live_tup > 0
+        ORDER BY n_live_tup DESC;
+'''
+
+for connection in (odata_connection, django_connection):
+    result = connection.execute(COUNT_ALL_SQL).fetchall()
+    pprint.pprint(result)
