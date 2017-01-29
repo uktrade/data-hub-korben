a1f336b5710726ebf08f8b2de9d2738a0e7812dc
bmcorser <bmcorser@gmail.com>
2017-01-29 11:55:10 +0100

    Docker on CircleCI doesn’t count as CircleCI?

--- test-tier0.sh
+++ test-tier0.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xe
 py.test -vv --tb=short test/tier0 --cov=korben && \
     wget -O codecov.sh https://codecov.io/bash && \
-    bash codecov.sh
+    bash codecov.sh -t ${CODECOV_TOKEN}
