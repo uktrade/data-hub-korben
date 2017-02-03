c2f3d03cef49d91479a1ffa1588f7c581831b6d5
bmcorser <bmcorser@gmail.com>
2017-02-03 11:07:12 +0000

    Mazing test

--- test/unit/coverage/test_webserver.py
+++ test/unit/coverage/test_webserver.py
@@ -4,10 +4,12 @@ from unittest.mock import Mock
 import importlib
 import os
 
+from cryptography.fernet import Fernet
 import sqlalchemy as sqla
+import redis
 
 from korben import config
-from korben.bau import webserver
+from korben.bau import webserver, sentry_client
 from korben.cdms_api.rest import api
 from korben.services import db
 
@@ -27,13 +29,25 @@ def test_get_app_no_overrides(mock_externals):
     webserver.get_app()
 
 
-def test_wsgi_app_constant(monkeypatch):
+def test_wsgi_app_constant(monkeypatch, mock_externals):
+    monkeypatch.setattr(sentry_client, 'SENTRY_CLIENT', Mock())
     mock_get = Mock()
     mock_get.return_value = ''
     monkeypatch.setattr(os.environ, 'get', mock_get)
-    setattr(config, 'cdms_base_url', '')
-    setattr(config, 'database_odata_url', 'HELLO MUM')
-    try:
-        importlib.reload(webserver)
-    except sqla.exc.ArgumentError as exc:
-        assert 'HELLO MUM' in str(exc)
+    temp_settings = {
+        'cdms_base_url': 'a',
+        'database_odata_url': 'HELLO MUM',
+        'database_url': 'HELLO MUM',
+        'cdms_username': 'd',
+        'cdms_password': 'e',
+        'cdms_base_url': 'f',
+        'cdms_cookie_path': 'g',
+        'cdms_cookie_key': Fernet.generate_key().decode('utf8'),
+    }
+    with config.temporarily(**temp_settings):
+        try:
+            importlib.reload(webserver)
+        except redis.exceptions.ConnectionError as exc:
+            assert 'Name or service not known' in str(exc)
+        except sqla.exc.ArgumentError as exc:
+            assert 'HELLO MUM' in str(exc)
