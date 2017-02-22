689c41202507b11f10991bfe21d87077a0626018
bmcorser <bmcorser@gmail.com>
2017-02-22 15:58:58 +0100

    Ressurect CookieStorage unit tests

--- test/tier1/cdms_api/test_cookie_storage.py
+++ test/tier1/cdms_api/test_cookie_storage.py
@@ -1,10 +1,12 @@
+import binascii
+import functools
 import os
 import pickle
 from unittest import mock
+from unittest import TestCase
 from cryptography.fernet import Fernet
 
-from django.conf import settings
-from django.test.testcases import TestCase
+from korben import config, services
 from django.core.exceptions import ImproperlyConfigured
 
 from korben.cdms_api.cookie_storage import CookieStorage
@@ -12,30 +14,31 @@ from korben.cdms_api.cookie_storage import CookieStorage
 
 class BaseCookieStorageTestCase(TestCase):
     def setUp(self):
-        self.fernet = Fernet(settings.CDMS_COOKIE_KEY)
-        if os.path.exists(settings.COOKIE_FILE):
-            os.remove(settings.COOKIE_FILE)
+        config.cdms_cookie_key = Fernet.generate_key()
+        self.fernet = Fernet(config.cdms_cookie_key)
+        if services.redis.get(config.cdms_cookie_path):
+            services.redis.delete(config.cdms_cookie_path)
 
     def write_cookie(self, cookie={}):
         ciphertext = self.fernet.encrypt(pickle.dumps(cookie))
-        with open(settings.COOKIE_FILE, 'wb') as f:
-            f.write(ciphertext)
+        services.redis.set(config.cdms_cookie_path, ciphertext)
 
     def assertCookieDoesNotExist(self):
-        self.assertFalse(os.path.exists(settings.COOKIE_FILE))
+        self.assertFalse(os.path.exists(config.cdms_cookie_path))
 
 
 class SetupStorageTestCase(BaseCookieStorageTestCase):
     def test_non_valid_key(self):
-        with self.settings(
-            CDMS_COOKIE_KEY=b'something'
-        ):
-            self.assertRaises(ImproperlyConfigured, CookieStorage)
+        with config.temporarily(cdms_cookie_key='something'):
+            cls_closed = functools.partial(
+                CookieStorage, config.cdms_cookie_path
+            )
+            self.assertRaises(config.ConfigError, cls_closed)
 
 
 class ReadCookieStorageTestCase(BaseCookieStorageTestCase):
     def test_returns_None_if_cookie_doesnt_exist(self):
-        storage = CookieStorage()
+        storage = CookieStorage(config.cdms_cookie_path)
         self.assertEqual(storage.read(), None)
 
     def test_returns_None_if_cookie_invalid(self):
@@ -45,10 +48,10 @@ class ReadCookieStorageTestCase(BaseCookieStorageTestCase):
         self.write_cookie()
 
         with mock.patch(
-            'korben.cdms_api.cookie_storage.open',
+            'korben.cdms_api.cookie_storage.services.redis_bytes.get',
             mock.mock_open(read_data='something')
         ):
-            storage = CookieStorage()
+            storage = CookieStorage(config.cdms_cookie_path)
             self.assertEqual(storage.read(), None)
 
         self.assertCookieDoesNotExist()
@@ -59,54 +62,43 @@ class ReadCookieStorageTestCase(BaseCookieStorageTestCase):
         """
         self.write_cookie()
         encrypted_cookie = Fernet(Fernet.generate_key()).encrypt(pickle.dumps('something'))
-
-        with mock.patch(
-            'korben.cdms_api.cookie_storage.open',
-            mock.mock_open(read_data=encrypted_cookie)
-        ):
-            storage = CookieStorage()
-            self.assertEqual(storage.read(), None)
+        services.redis_bytes.set(config.cdms_cookie_path, encrypted_cookie)
+        storage = CookieStorage(config.cdms_cookie_path)
+        self.assertEqual(storage.read(), None)
 
         self.assertCookieDoesNotExist()
 
     def test_read(self):
         cookie = {'key': 'value'}
         self.write_cookie(cookie)
-
-        storage = CookieStorage()
-        self.assertEqual(
-            storage.read(), cookie
-        )
+        storage = CookieStorage(config.cdms_cookie_path)
+        self.assertEqual(storage.read(), cookie)
 
 
 class WriteCookieStorageTestCase(BaseCookieStorageTestCase):
     def test_write(self):
         cookie = {'key': 'value'}
-
-        storage = CookieStorage()
+        storage = CookieStorage(config.cdms_cookie_path)
         storage.write(cookie)
-
-        with open(settings.COOKIE_FILE, 'rb') as f:
-            ciphertext = self.fernet.decrypt(f.read())
-            cookie_on_fs = pickle.loads(ciphertext)
-
-        self.assertEqual(cookie_on_fs, cookie)
+        ciphertext = self.fernet.decrypt(
+            services.redis_bytes.get(config.cdms_cookie_path)
+        )
+        cookie_in_redis = pickle.loads(ciphertext)
+        self.assertEqual(cookie_in_redis, cookie)
 
 
 class ExistsCookieStorageTestCase(BaseCookieStorageTestCase):
     def test_doesnt_exists(self):
-        self.assertFalse(os.path.exists(settings.COOKIE_FILE))
+        self.assertFalse(services.redis.get(config.cdms_cookie_path))
 
     def test_exists(self):
         self.write_cookie()
-        self.assertTrue(os.path.exists(settings.COOKIE_FILE))
+        self.assertTrue(services.redis.get(config.cdms_cookie_path))
 
 
 class ResetCookieStorageTestCase(BaseCookieStorageTestCase):
     def test_reset(self):
         self.write_cookie()
-
-        storage = CookieStorage()
+        storage = CookieStorage(config.cdms_cookie_path)
         storage.reset()
-
-        self.assertFalse(os.path.exists(settings.COOKIE_FILE))
+        self.assertFalse(services.redis.get(config.cdms_cookie_path))
